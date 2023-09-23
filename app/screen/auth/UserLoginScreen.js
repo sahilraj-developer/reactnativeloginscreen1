87d359f8bdff9f1162241bc7b60882a46b5e21f0
sahil <sahil27raj@gmail.com>
2023-09-23 14:01:16 +0530

    only login screen

---
+++ app/screen/auth/UserLoginScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UserLoginScreen = () => {
+  return (
+    <View>
+      <Text>UserLoginScreen</Text>
+    </View>
+  )
+}
+
+export default UserLoginScreen
